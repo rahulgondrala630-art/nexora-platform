35cb65fd9b19c948240742fdee7e243108d47bbc
Rahul Gondrala <rahulgondrala630@gmail.com>
2026-09-21 16:24:50 -0500

    Delete NexoraApplication.java

--- NexoraApplication.java
+++
@@ -1,9 +0,0 @@
-package com.nexora.platform;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class NexoraApplication {
-    public static void main(String[] args) { SpringApplication.run(NexoraApplication.class, args); }
-}
